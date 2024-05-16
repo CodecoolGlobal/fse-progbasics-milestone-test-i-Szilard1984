e31d34fccfa8ca3671fe1be5a51d8cc0655cc99e
Szilard1984 <bardoc.szilard@gmail.com>
2024-05-16 12:22:11 +0200

    error corrected in AdultAge

--- solutions/averageAgeOfAdults.js
+++ solutions/averageAgeOfAdults.js
@@ -16,13 +16,15 @@ You do not have to deal with the case, when there are only underage users in the
 */
 
 function averageAgeOfAdults(users) {
+  let adultCounter = 0
   let addAges = 0
   for (const user of users){
     if (user.age >= 18){
+      adultCounter++
       addAges += user.age 
     }
   }
-  return addAges/users.length
+  return addAges/adultCounter
 }
 
 module.exports = averageAgeOfAdults;
